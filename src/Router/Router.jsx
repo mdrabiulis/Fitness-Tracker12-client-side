ae81d6f09f4a605a972dccbba3a0fd99318ba241
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-27 18:14:06 +0600

    Layout/Router setapp

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+        {}
+    ],
+  },
+]);
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// import { ThemeProvider } from '@material-tailwind/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router/Router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <ThemeProvider> */}
-    <App />
-    {/* </ThemeProvider> */}
-  </React.StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
